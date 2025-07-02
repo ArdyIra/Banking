3f425db16ed379c7930b2208223f33f085c76eb5
ArdyIra <gvladder@gmail.com>
2025-07-02 16:15:57 +0300

    Done the first task

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,5 +1,44 @@
-public class Main {
+public class BankAccount {
+    private double balance;
+    public BankAccount() {
+        this.balance = 0;
+    }
+
+    public BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        this.balance = this.balance + amount;
+    }
+
+    public void withdraw(double amount) {
+        this.balance = this.balance - amount;
+    }
+
+    public void printBalance() {
+        System.out.println("The current balance is: " + this.balance);
+    }
+
+    public void transferBalance(double amount, BankAccount transferTo){
+        this.balance = this.balance - amount;
+        transferTo.deposit(amount);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BankAccount acc1 = new BankAccount(250);
+        BankAccount acc2 = new BankAccount();
+
+        acc1.printBalance();
+
+        acc1.deposit(50);
+        acc1.printBalance();
+
+        acc1.withdraw(100);
+        acc1.printBalance();
+        acc1.transferBalance(75, acc2);
+
+        acc1.printBalance();
+        acc2.printBalance();
+
     }
 }
